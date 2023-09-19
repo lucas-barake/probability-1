92a3b95cd85bfb6a52560221193e073e9bb6a0d2
lucas <86905052+lucas-barake@users.noreply.github.com>
2023-09-18 22:06:59 -0500

    docs: update README

--- README.md
+++ README.md
@@ -13,3 +13,10 @@ bun run main
 ```
 
 This project was created using `bun init` in bun v1.0.0. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+
+Instructions:
+
+- [x] Cada estudiante del grupo debe seleccionar 2 ciudades (no se puede repetir ciudad) y realizar, para cada ciudad, un histograma de las variables velocidad del viento y temperatura. Basado en el coeficiente de variación, ¿Cuál ciudad presenta mayor variabilidad? En este apartado también se debe hacer una comparación entre las dos ciudades con un diagrama de caja bigotes. Es necesario justificar la selección de los lugares escogidos. También es necesario justificar por qué escogieron estas ciudades.
+- [x] Para cada ciudad elegida, calcular los parámetros 𝑘 y 𝑐 de la distribución de Weibull. Para hacer estos cálculos use las ecuaciones 3 y 4 descritas arriba y sustituya los valores obtenidos en la función de densidad de probabilidad, 𝑓(𝑣) (ecuación 1).
+- [ ] Graficar la distribución de probabilidad, 𝑓(𝑣), obtenida en el ítem anterior y compararla con el respectivo histograma de la velocidad del viento (obtenido en el punto 1) para las ciudades seleccionadas. A partir de las gráficas, ¿qué se puede concluir sobre el comportamiento de las variables? Nota: se debe tener presente que la forma de la distribución debe estar completa.
+- [ ] Para cada ciudad seleccionada, obtener el valor de velocidad del viento más probable y el valor de la velocidad del viento que produciría la máxima energía eólica (ecuaciones 5 y 6). Al comparar los valores para las ciudades seleccionadas, ¿cuál de ellos tiene más probabilidad de generar mayor energía eólica?, ¿por qué?
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 import * as simpleStatistics from "simple-statistics";
 import { WeibullParams, weibullMLE } from "./weibull-mle";
-import fs from "fs";
 import * as mathjs from "mathjs";
+import { ChartJSNodeCanvas } from "chartjs-node-canvas";
 
 const variables = ["Velocity_of_the_Wind", "Temperature"] as const;
 const cities = ["SAN GIL", "POPAYÁN"];
@@ -46,49 +46,6 @@ function getWhiskerPlotData(
   };
 }
 
-function createHistogram(data: City[], variable: (typeof variables)[number], city: string) {
-  const filteredData = data.filter((d) => d.Municipality === city);
-
-  const windSpeeds = filteredData.map((d) => d.Velocity_of_the_Wind);
-  const weibullParams = weibullMLE(windSpeeds);
-
-  const values = filteredData.map((d) => d[variable]);
-
-  const probabilityDistribution = getProbabilityDistribution(weibullParams, values);
-
-  const mean = simpleStatistics.mean(values);
-  const standardDeviation = simpleStatistics.standardDeviation(values);
-
-  const coefficientOfVariation = (standardDeviation / mean) * 100;
-
-  const trace = {
-    x: values,
-    type: "histogram",
-    xbins: {
-      size: (Math.max(...values) - Math.min(...values)) / 20, // fixed bin size
-    },
-  };
-
-  const layout = {
-    title: `Histogram of ${variable} for ${city} (CV = ${coefficientOfVariation.toFixed(2)}%)`,
-    xaxis: {
-      title: variable,
-    },
-    yaxis: {
-      title: "Frequency",
-    },
-    margin,
-  };
-
-  const figure = { data: [trace], layout };
-
-  return {
-    ...figure,
-    weibull_params: weibullParams,
-    probability_distribution: probabilityDistribution,
-  };
-}
-
 function getProbabilityDistribution(weibullParams: WeibullParams, values: number[]) {
   // create an array of x values from 0 to the maximum value in the data
   const xValues = mathjs.range(0, mathjs.max(values), 0.1).toArray();
@@ -108,26 +65,153 @@ function getProbabilityDistribution(weibullParams: WeibullParams, values: number
   return { x: xValues, y: yValues };
 }
 
+async function createHistogram(data: City[], variable: (typeof variables)[number], city: string) {
+  // create a new instance of chartjs-node-canvas with width and height options
+  const chartJSNodeCanvas = new ChartJSNodeCanvas({ width: 800, height: 600 });
+
+  // define the chart configuration object
+  const configuration = {
+    type: "scatter", // use scatter type for the chart
+    data: {
+      // define the data for the chart
+      datasets: [
+        // use one dataset for the histogram and one for the probability distribution
+        {
+          label: `Histogram of ${variable} for ${city}`, // label for the histogram dataset
+          data: data
+            .filter((d) => d.Municipality === city)
+            .map((d) => ({ x: d[variable], y: null })), // map the data to x and y coordinates
+          backgroundColor: "rgba(255,99,132,0.2)", // set the background color for the histogram bars
+          borderColor: "rgba(255,99,132,1)", // set the border color for the histogram bars
+          borderWidth: 1, // set the border width for the histogram bars
+          showLine: false, // do not show a line connecting the points
+          pointRadius: 0, // set the point radius to zero to hide the points
+          xAxisID: "histogram", // use the histogram axis for the x values
+          yAxisID: "frequency", // use the frequency axis for the y values
+        },
+        {
+          label: `Probability Distribution of ${variable} for ${city}`, // label for the probability distribution dataset
+          data: getProbabilityDistribution(
+            weibullMLE(
+              data.filter((d) => d.Municipality === city).map((d) => d.Velocity_of_the_Wind)
+            ),
+            data.filter((d) => d.Municipality === city).map((d) => d[variable])
+          ).x.map((x, i) => ({
+            x,
+            y: getProbabilityDistribution(
+              weibullMLE(
+                data.filter((d) => d.Municipality === city).map((d) => d.Velocity_of_the_Wind)
+              ),
+              data.filter((d) => d.Municipality === city).map((d) => d[variable])
+            ).y[i],
+          })), // map the x and y values of the probability distribution to coordinates
+          backgroundColor: "rgba(54,162,235,0.2)", // set the background color for the probability distribution line
+          borderColor: "rgba(54,162,235,1)", // set the border color for the probability distribution line
+          borderWidth: 3, // set the border width for the probability distribution line
+          showLine: true, // show a line connecting the points
+          pointRadius: 0, // set the point radius to zero to hide the points
+          xAxisID: "histogram", // use the histogram axis for the x values
+          yAxisID: "probability", // use the probability axis for the y values
+        },
+      ],
+    },
+    options: {
+      // define the options for the chart
+      title: {
+        // set the title options
+        display: true, // display the title
+        text: `Histogram and Probability Distribution of ${variable} for ${city}`, // set the title text
+        fontSize: 24, // set the title font size
+      },
+      scales: {
+        // define the scales for the axes
+        histogram: {
+          // define the histogram axis
+          type: "linear", // use a linear type for the axis
+          position: "bottom", // position the axis at the bottom
+          scaleLabel: {
+            // set the scale label options
+            display: true, // display the scale label
+            labelString: variable, // set the scale label text
+            fontSize: 18, // set the scale label font size
+          },
+          ticks: {
+            // set the tick options
+            beginAtZero: true, // start the ticks at zero
+            fontSize: 16, // set the tick font size
+          },
+        },
+        frequency: {
+          // define the frequency axis
+          type: "linear", // use a linear type for the axis
+          position: "left", // position the axis at the left
+          scaleLabel: {
+            // set the scale label options
+            display: true, // display the scale label
+            labelString: "Frequency", // set the scale label text
+            fontSize: 18, // set the scale label font size
+          },
+          ticks: {
+            // set the tick options
+            beginAtZero: true, // start the ticks at zero
+            fontSize: 16, // set the tick font size
+          },
+        },
+        probability: {
+          // define the probability axis
+          type: "linear", // use a linear type for the axis
+          position: "right", // position the axis at the right
+          scaleLabel: {
+            // set the scale label options
+            display: true, // display the scale label
+            labelString: "Probability Density", // set the scale label text
+            fontSize: 18, // set the scale label font size
+          },
+          ticks: {
+            // set the tick options
+            beginAtZero: true, // start the ticks at zero
+            fontSize: 16, // set the tick font size
+          },
+        },
+      },
+      legend: {
+        // define the legend options
+        display: true, // display the legend
+        labels: {
+          // set the label options
+          fontSize: 18, // set the label font size
+        },
+      },
+    },
+    margin, // use the margin object for the chart margin
+  };
+
+  // render the chart as a PNG image and return it as a buffer
+  const image = await chartJSNodeCanvas.renderToBuffer(configuration);
+
+  // return the image buffer and the weibull parameters
+  return {
+    image,
+    weibull_params: weibullMLE(
+      data.filter((d) => d.Municipality === city).map((d) => d.Velocity_of_the_Wind)
+    ),
+  };
+}
+
 const data = await Bun.file(`${__dirname}/data.json`).json<City[]>();
 const parsedData = data.map((d) => city.parse(d));
 
 for (const variable of variables) {
   for (const city of cities) {
-    const histogram = createHistogram(parsedData, variable, city);
-    const dirName = `${city}_${variable}`;
-
-    // create directory if it doesn't exist
-    if (!fs.existsSync(`${__dirname}/output/${dirName}`)) {
-      fs.mkdirSync(`${__dirname}/output/${dirName}`);
-    }
+    const histogram = await createHistogram(parsedData, variable, city); // use await to get the histogram result
 
     Bun.write(
-      `${__dirname}/output/${dirName}/histogram-data-x.csv`,
-      ["A", ...histogram.data[0].x].join("\n")
+      `${__dirname}/output/${city}_${variable}/histogram.png`,
+      histogram.image // write the image buffer to a PNG file
     );
 
     Bun.write(
-      `${__dirname}/output/${dirName}/data.json`,
+      `${__dirname}/histograms/${city}_${variable}.json`,
       JSON.stringify(
         {
           ...histogram,
